26d576243224b52b44c6861521097974b818085d
Luiz Fernando <fernandosena1@icloud.com>
2024-12-02 13:51:03 -0300

    initial commit

---
+++ src/main/java/com/salao/cabelereiro/CabelereiroApplication.java
@@ -0,0 +1,13 @@
+package com.salao.cabelereiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabelereiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabelereiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salao/cabelereiro/CabelereiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salao.cabelereiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabelereiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
